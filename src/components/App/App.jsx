5ac0f98fb676bc82f1dbfe26dc286ed20729ac1c
Alla Yevlampiieva <modan1679@gmail.com>
2023-08-31 19:27:09 +0300

    Updated components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -12,12 +12,6 @@ const localStorageKey = 'my-contacts';
 export class App extends Component {
   state = {
     contacts: [],
-    // contacts: [
-    //   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    //   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    //   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    //   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    // ],
     filter: '',
   };
 
@@ -28,13 +22,12 @@ export class App extends Component {
       number,
     };
 
-    if (this.state.contacts.some(contacts => contacts.name === name)) {
+    if (this.state.contacts.some(contacts => contacts.name.toLowerCase() === name.toLowerCase())) {
       alert(`${name} is already in contacts`);
-    } else {
-      this.setState(({ contacts }) => ({
+      return;
+    } this.setState(({ contacts }) => ({
         contacts: [contact, ...contacts],
-      }));
-    }
+      }));    
   };
 
   deleteContact = contactId => {
